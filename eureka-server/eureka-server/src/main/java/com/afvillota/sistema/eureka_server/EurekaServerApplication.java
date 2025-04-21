471892025bc09c8ce62411d1735f748bae6ef405
Felipe-Santander <felipevillota2002@gmail.com>
2025-04-20 21:36:12 -0500

    Implement Eureka Server microservice

---
+++ eureka-server/eureka-server/src/main/java/com/afvillota/sistema/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.afvillota.sistema.eureka_server; // <--- CORRECTO (o el que tengas generado)
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
